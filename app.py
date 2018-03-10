08f9a985467985d790bd4be4b858e554b06ac9e2
Sarvesh Yadav <sarvesh.y305@gmail.com>
2018-03-10 23:56:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
 from flask import Flask, jsonify
-from flask_cors import CORS, cross_origin
 
 
 app = Flask(__name__)
-cors = CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
 
 
 @app.route('/')
