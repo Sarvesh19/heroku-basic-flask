102fd28be664f7c6bc04398a6b802e05b9085b05
Sarvesh Yadav <sarvesh.y305@gmail.com>
2018-03-11 00:02:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
 from flask import Flask, jsonify
+from flask_cors import CORS, cross_origin
 
 
 app = Flask(__name__)
-
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/')
+@cross_origin()
 def homepage():
     
     return jsonify( { 'tasks': "hello homePage" } )
 
 
 @app.route('/getUser')
+@cross_origin()
 def getUser():
 
      return jsonify( {
