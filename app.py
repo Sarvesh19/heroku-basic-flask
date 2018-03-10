abc9ef80404ff6b56b6e77b396b0dca0dd41afa9
Sarvesh Yadav <sarvesh.y305@gmail.com>
2018-03-10 20:36:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
-from flask import Flask, request, jsonify
+from flask import Flask
 from datetime import datetime
-from flask_restful import Resource, Api
-from json import dumps
 
 app = Flask(__name__)
 
