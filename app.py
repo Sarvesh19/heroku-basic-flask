b608161272eb555267de41874e3ee511ca0682c7
Sarvesh Yadav <sarvesh.y305@gmail.com>
2018-03-10 23:17:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,12 +11,15 @@ def homepage():
 
 @app.route('/getUser')
 def getUser():
-    
-    return jsonify( {
+
+    response = jsonify( {
     "username": "sarvesh",
     "email": "sarvesh.y305@gmail.com",
     "id": "Sarvesh19"
-} )
+    } 
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+    )
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
