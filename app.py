52e6c2bde5063c6edbbab7bded07aec89e432d6b
Sarvesh Yadav <sarvesh.y305@gmail.com>
2018-03-10 21:38:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 
 app = Flask(__name__)
@@ -6,9 +6,17 @@ app = Flask(__name__)
 @app.route('/')
 def homepage():
     
-    return 'hello'
+    return jsonify( { 'tasks': "hello homePage" } )
 
 
+@app.route('/getUser')
+def getUser():
+    
+    return jsonify( {
+    "username": "admin",
+    "email": "admin@localhost",
+    "id": 42
+} )
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
